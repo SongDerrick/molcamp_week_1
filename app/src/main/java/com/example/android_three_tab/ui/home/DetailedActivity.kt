640d12832550e8ad98ff8d8859742f7f2b80db40
SongDerrick <sd990307@gmail.com>
2023-07-01 16:47:52 +0900

    Action Bar modification

--- app/src/main/java/com/example/android_three_tab/ui/home/DetailedActivity.kt
+++ app/src/main/java/com/example/android_three_tab/ui/home/DetailedActivity.kt
@@ -2,6 +2,7 @@ package com.example.android_three_tab.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageView
 import com.example.android_three_tab.R
 
@@ -11,6 +12,10 @@ class DetailedActivity : AppCompatActivity() {
         setContentView(R.layout.activity_detailed)
         val position = intent.getIntExtra("position", -1)
         // val bundle : Bundle?= intent.extras
+
+        //window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+
+        supportActionBar?.hide()
 
         val imageView : ImageView = findViewById(R.id.imageView)
         imageView.setImageResource(position)
