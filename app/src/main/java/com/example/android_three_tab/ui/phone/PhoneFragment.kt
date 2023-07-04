e0cb5a599a8d4fcd46b0c5c204094666e3a208b2
장동우 <2019076144@hanyang.ac.kr>
2023-07-04 17:47:56 +0900

    JD0704 commit

--- app/src/main/java/com/example/android_three_tab/ui/phone/PhoneFragment.kt
+++ app/src/main/java/com/example/android_three_tab/ui/phone/PhoneFragment.kt
@@ -1,17 +1,22 @@
 package com.example.android_three_tab.ui.phone
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView.OnItemClickListener
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.android_three_tab.databinding.FragmentPhoneBinding
 import com.example.android_three_tab.ui.dashboard.PhoneData
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 
-class PhoneFragment : Fragment() {
+class PhoneFragment : Fragment(), PhoneAdapter.OnItemClickListener {
 
     private var _binding: FragmentPhoneBinding? = null
 
@@ -20,7 +25,7 @@ class PhoneFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
-
+    var tempIndexx: Int = 5
     private val dataList = mutableListOf(
         PhoneData("Jang", "010-1234-1234"),
         PhoneData("Song", "010-1241-1313"),
@@ -37,15 +42,17 @@ class PhoneFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-//        val dashboardViewModel =
-//            ViewModelProvider(this).get(DashboardViewModel::class.java)
-
         _binding = FragmentPhoneBinding.inflate(inflater, container, false)
         val view = binding.root
 
-        adapter = PhoneAdapter(dataList as ArrayList<PhoneData>)
+        val dividerItemDecoration = DividerItemDecoration(binding.rcvPhone.context, LinearLayoutManager(requireContext()).orientation)
+        binding.rcvPhone.addItemDecoration(dividerItemDecoration)
+
+        adapter = PhoneAdapter(dataList as ArrayList<PhoneData>, this)
         binding.rcvPhone.layoutManager = LinearLayoutManager(requireContext())
         binding.rcvPhone.adapter = adapter
+
+
 
         val fab_btn: FloatingActionButton = binding.fabMain
         fab_btn.setOnClickListener {
@@ -65,12 +72,17 @@ class PhoneFragment : Fragment() {
         dialog.show(parentFragmentManager, "AddPhoneDialog")
     }
 
+    override fun onItemClick(position:Int) {
+        val item = dataList[position]
+        val phoneN = item.phoneNum
+        val dialIntent = Intent(Intent.ACTION_DIAL)
+        dialIntent.data = Uri.parse("tel:$phoneN")
+        startActivity(dialIntent)
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
-
-
 
 }
